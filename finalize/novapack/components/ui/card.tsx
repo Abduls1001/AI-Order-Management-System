c98678e91a8698ba991af6f7611a1c5f0ab3cb45
Abduls1001 <abdulrehmansarwar594@gmail.com>
2026-07-14 23:22:34 +0500

    commit

--- finalize/novapack/components/ui/card.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export function Card({ className, children }: { className?: string; children: React.ReactNode }) {
-  return (
-    <div className={cn("border border-line bg-white rounded-sm", className)}>
-      {children}
-    </div>
-  );
-}
